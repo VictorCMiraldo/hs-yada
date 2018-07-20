7470f48662d49f6ccde1607ec8bfc25f4e178de6
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2018-07-20 16:12:10 +0200

    More Treefix play

--- src/Tree23_hashset.hs
+++ src/Tree23_hashset.hs
@@ -4,21 +4,72 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 module Tree23_hashset where
 
 import Control.Arrow ((***),(&&&))
 import Data.Hashable
+import Data.Maybe (mapMaybe)
 import Control.Applicative
 import Control.Monad
 import Control.Monad.State
 
-import Tree23
+import Tree23 hiding (Hole)
 import WordTrie
 import Generics.MRSOP.Base
 import Generics.MRSOP.Opaque
 import Generics.MRSOP.Util hiding (Cons , Nil)
 import Generics.MRSOP.Treefix
 
+type MyPath = Way CodesTree23 (I Z)
+type DiffM = State (WordTrie ([MyPath] , [MyPath]))
+
+enumNP :: NP f xs -> [NS f xs]
+enumNP NP0 = []
+enumNP (fx :* fxs) = Here fx : map There (enumNP fxs)
+
+trieFromTree :: Tree23 -> WordTrie [MyPath]
+trieFromTree t = go trieEmpty [(HoleW , t)]
+  where
+    go :: WordTrie [MyPath] -> [(MyPath , Tree23)] -> WordTrie [MyPath]
+    go tr []        = tr
+    go tr ((_ , Leaf):ts) = go tr ts
+{-
+    go tr ((h , t):ts)    = go (trieAdd [h] (h:) (octets $ hash t) tr)
+                               (ts ++ map (mergePath h *** id) (zip [0..] $ children t))
+-}
+    go tr ((h , t):ts) = case sop (sfrom $ into @FamTree23 t) of
+      Tag c ps -> go (trieAdd [h] (h :) (octets $ hash t) tr)
+                     (ts ++ mapMaybe (goNS _ c) (enumNP ps))
+
+    
+
+    isRec :: NA Singl (El FamTree23) ix -> Maybe Tree23
+    isRec = _
+
+    constHole :: NA Singl (El FamTree23) a -> Maybe (Way CodesTree23 a)
+    constHole (NA_I _) = return HoleW
+    constHole _        = Nothing
+
+    goNS :: IsNat c
+         => (MyPath -> MyPath) -> Constr (Lkup Z CodesTree23) c
+         -> NS (NA Singl (El FamTree23)) (Lkup c (Lkup Z (CodesTree23)))
+         -> Maybe (MyPath , Tree23)
+    goNS h c ns = do aux <- mapNSM constHole ns
+                     t   <- elimNS isRec ns
+                     return (h (Follow c aux) , t)
+{-
+    isRec uptohere (There rest) = isRec uptohere rest
+    isRec uptohere (Here  (NA_K _))  = Nothing
+    isRec uptohere (Here  (NA_I el))
+      = case getElSNat el of
+          SZ -> Just (HoleW , unEl el)
+ 
+    mergePath :: MyPath -> Integer -> MyPath
+    mergePath p i = _
+
+-}
+{-
 -- |I'll keep a set that counts how many times each common subtree
 --  still shows up (and the path to it) in the source and destination.
 --
@@ -64,33 +115,12 @@ treeExtract (p:ps) t
 --    . map (flip treeExtract t1) ps =~= map (flip treeExtract t2) qs
 --
 --  l1 =~= l2 iff l1 ++ l2 == replicate n x for some n and x
-
+-}
 mt1 , mt2 , mt3 , mt4 :: Tree23
 mt1 = Node2 10 Leaf Leaf
 mt2 = Node3 20 Leaf mt1 Leaf
 mt3 = Node2 30 Leaf Leaf
 mt4 = Node3 50 mt1 mt2 mt3
-
-
--- PROBLEM:
---
--- I want to define the following function:
---
--- getCtx :: (syncs : WordTrie ([Path] , [Path]))
---        -> Tx Singl FamTree23 CodesTree23 (Idx Tree23) (getAllSyncs syncs)
---
--- In fact; we only know the type of the Treefix at run-time. Hence,
--- we pack it in an existential
-data TxE :: (kon -> *) -> [*] -> [[[Atom kon]]] -> Nat -> * where
-  TxE :: Tx ki fam codes i js -> TxE ki fam codes i
-
-getCtx :: WordTrie ([Path] , [Path]) -> Tree23
-       -> TxE Singl FamTree23 CodesTree23 Z
-getCtx wd tr
-  = case trieTreeLkup tr wd of
-      -- Recurse down the children of tr
-      Nothing        -> _
-      Just (ps , qs) -> _
 
 {-
   
